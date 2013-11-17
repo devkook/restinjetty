a6999cd4c11cf3b1408d12824572bece36d02f7b
devkook <devinkook@gmail.com>
2013-11-17 14:14:44 +0900

    우주시간 측정 및 테스트 코드 작성 - 이를 위해 jetty 서버의 start, join 부분을 분리하여 테스트 코드에서 검증 가능하도록 수

--- src/main/java/devkook/study/jetty/JerseyJettyLauncher.java
+++ src/main/java/devkook/study/jetty/JerseyJettyLauncher.java
@@ -5,32 +5,41 @@ import java.io.IOException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 public class JerseyJettyLauncher {
     private Server server;
-    private int port;
+    private int port = 8765;
     
     public static void main(String[] args) {
         JerseyJettyLauncher launcher = new JerseyJettyLauncher();
-        launcher.port = Integer.parseInt(System.getProperty("port", "7788"));
         
         try {
-            
             launcher.start();
+            launcher.server.join();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void setPort(){
+    	this.port = Integer.parseInt(System.getProperty("port", "8765"));
     }
     
     public void start() throws IOException {
         server = createJettyServer();
         try {
-            server.start();
-            server.join();
+            server.start();            
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    public void stop() throws Exception{
+    	server.stop();
     }
 
     protected Server createJettyServer() {
--- src/test/java/devkook/study/jetty/JerseyJettyLauncherTest.java
+++ src/test/java/devkook/study/jetty/JerseyJettyLauncherTest.java
@@ -2,19 +2,41 @@ package devkook.study.jetty;
 
 import static org.junit.Assert.*;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
+
 import org.junit.Test;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class JerseyJettyLauncherTest {
-    @Test
-    public void 저지레스트테스트() throws Exception {
+    
+	@Test
+    public void 우주시간확인() throws Exception {
         //선행조건(given)
-        String[] args = {};
-        JerseyJettyLauncher.main(args);
-
+        JerseyJettyLauncher jjl = new JerseyJettyLauncher();
+        System.setProperty("port", "8765");
+        jjl.setPort();
+        jjl.start();
+        
         //기능수행(when)
+        ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		
+		String uri = "http://localhost:8765/api/SatelliteTower/ping";
+		WebResource webResource = client.resource(UriBuilder.fromUri(uri).build());
+		ClientResponse response = webResource.post(ClientResponse.class);
+		String utime = response.getEntity(String.class);
+		System.out.println("Response:" + utime);
+			
+        jjl.stop();
 
         //결과확인(then)
-        fail("때때로 실패하지 않으면, 안이하게 살고 있다는 확실한 증거이다");
+        assertTrue("우주시간 확인", utime.startsWith("This MHIS filed cosmology. Roger. Space-time:"));
     }
 }
